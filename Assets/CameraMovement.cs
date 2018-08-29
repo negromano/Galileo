6aad8ae558095c89ea91ecf0acfae371dc975a09
Juan Pablo Rodriguez Morales <uncorreodeinternet@gmail.com>
2018-08-29 11:31:08 -0500

    First Commit, player now has idle and running animations and first level platforms an colliders are workng

---
+++ Assets/CameraMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+    public GameObject player;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(player.transform.position.x<0)
+            transform.position = new Vector3(0, transform.position.y, -10f);
+        else if (player.transform.position.x>74)
+            transform.position = new Vector3(74, transform.position.y, -10f);
+        else
+            transform.position = new Vector3(player.transform.position.x, transform.position.y, -10f);
+	}
+}
---
+++ Assets/Movement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public CharacterController2D controller;
+    public float speed = 80;
+    private float horizontalMove;
+    public Animator animator;
+    bool jump;
+	
+	// Update is called once per frame
+	void Update () {
+        horizontalMove = Input.GetAxisRaw("Horizontal")*speed;
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+        if (Input.GetKey(KeyCode.W)) {
+            jump = true;
+        }
+	}
+
+    void FixedUpdate() {
+        controller.Move(horizontalMove*Time.fixedDeltaTime,false, jump);
+        animator.SetBool("Jump", jump);
+        jump = false;
+    }
+}
